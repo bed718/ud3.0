7d3bc516f241c7ec868375dada77cb3861c0bb4a
Bryan Edwards <bryan@yahoo.com>
2015-08-30 18:29:08 -0700

    initial commit

---
+++ sites/all/themes/base/templates/node--post--teaser.tpl.php
@@ -0,0 +1,21 @@
+
+<div class="teaser post">
+	<div class="main">
+		<div class="image image-teaser"><?php print $cover_image; ?></div>
+	</div>
+	<div class="side">
+		<div class="header">
+			<h2><?php print $title_link; ?></h2>
+			<div class="meta"><?php print $created; ?> </div>
+		</div>
+		<div class="body">
+			<?php print $summary; ?>
+			
+			<div class="read-more">
+				<a href="<?php echo $path ?>">Read More</a>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
